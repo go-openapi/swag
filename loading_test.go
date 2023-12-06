4de06766517d82177ff1189f0b12530a19d9cb18
fredbi <fredbi@yahoo.com>
2023-12-06 14:01:07 +0100

    fix: windows support for loading files (#71)
    On windows, there are quite a few specifics when it comes to resolving
    a local file. This complexity stemmed from the desire to support
    common, albeit invalid URI constructs for windows.
    
    Overall, the original sin of this function has been to ignore the host
    part of an URI, but now we can't really break this behavior.
    
    * fixes #72
    * refactored the local path pre-processing, so the windows-specifics
      appear more clearly
    * fixed a few inconsistencies on windows file URIs and guarded against a panic
    
    * test: refactored tests for the local file strategy, now table-driven and
      more systematic
    * doc: added detailed documentation to LoadStrategy()
    * ci: re-enacted tests on windows
    * ci: temporarily muted past linting issues: relinting is deferred to another PR
    
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- loading_test.go
+++ loading_test.go
@@ -17,9 +17,11 @@ package swag
 import (
 	"net/http"
 	"net/http/httptest"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -31,9 +33,8 @@ const (
 )
 
 func TestLoadFromHTTP(t *testing.T) {
-
 	_, err := LoadFromFileOrHTTP("httx://12394:abd")
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
@@ -41,7 +42,7 @@ func TestLoadFromHTTP(t *testing.T) {
 	defer serv.Close()
 
 	_, err = LoadFromFileOrHTTP(serv.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	ts2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusOK)
@@ -50,7 +51,7 @@ func TestLoadFromHTTP(t *testing.T) {
 	defer ts2.Close()
 
 	d, err := LoadFromFileOrHTTP(ts2.URL)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, []byte("the content"), d)
 
 	ts3 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -65,21 +66,21 @@ func TestLoadFromHTTP(t *testing.T) {
 
 	// no auth
 	_, err = LoadFromFileOrHTTP(ts3.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	// basic auth, invalide credentials
 	LoadHTTPBasicAuthUsername = validUsername
 	LoadHTTPBasicAuthPassword = invalidPassword
 
 	_, err = LoadFromFileOrHTTP(ts3.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	// basic auth, valid credentials
 	LoadHTTPBasicAuthUsername = validUsername
 	LoadHTTPBasicAuthPassword = validPassword
 
 	_, err = LoadFromFileOrHTTP(ts3.URL)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	ts4 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		myHeaders := r.Header[sharedHeaderKey]
@@ -99,12 +100,12 @@ func TestLoadFromHTTP(t *testing.T) {
 	defer ts4.Close()
 
 	_, err = LoadFromFileOrHTTP(ts4.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	LoadHTTPCustomHeaders[sharedHeaderKey] = sharedHeaderValue
 
 	_, err = LoadFromFileOrHTTP(ts4.URL)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	// clean up for future tests
 	LoadHTTPBasicAuthUsername = ""
@@ -114,29 +115,28 @@ func TestLoadFromHTTP(t *testing.T) {
 
 func TestLoadHTTPBytes(t *testing.T) {
 	_, err := LoadFromFileOrHTTP("httx://12394:abd")
-	assert.Error(t, err)
+	require.Error(t, err)
 
-	serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+	serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
 	}))
 	defer serv.Close()
 
 	_, err = LoadFromFileOrHTTP(serv.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 
-	ts2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+	ts2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
 		rw.WriteHeader(http.StatusOK)
 		_, _ = rw.Write([]byte("the content"))
 	}))
 	defer ts2.Close()
 
 	d, err := LoadFromFileOrHTTP(ts2.URL)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, []byte("the content"), d)
 }
 
 func TestLoadStrategy(t *testing.T) {
-
 	loader := func(p string) ([]byte, error) {
 		return []byte(yamlPetStore), nil
 	}
@@ -152,49 +152,188 @@ func TestLoadStrategy(t *testing.T) {
 	defer serv.Close()
 
 	s, err := YAMLDoc(serv.URL)
-	assert.NoError(t, err)
-	assert.NotNil(t, s)
+	require.NoError(t, err)
+	require.NotNil(t, s)
 
-	ts2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+	ts2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
 		_, _ = rw.Write([]byte("\n"))
 	}))
 	defer ts2.Close()
 	_, err = YAMLDoc(ts2.URL)
-	assert.Error(t, err)
+	require.Error(t, err)
 }
 
 func TestLoadStrategyFile(t *testing.T) {
 	const (
-		thisIsIt = "thisIsIt"
+		thisIsIt    = "thisIsIt"
+		thisIsNotIt = "not it"
 	)
 
-	called, pth := false, ""
-	loader := func(p string) ([]byte, error) {
-		called = true
-		pth = p
-		return []byte(thisIsIt), nil
-	}
-	remLoader := func(p string) ([]byte, error) {
-		return []byte("not it"), nil
+	type strategyTest struct {
+		Title           string
+		Path            string
+		Expected        string
+		ExpectedWindows string
+		ExpectError     bool
 	}
 
-	ld := LoadStrategy("blah", loader, remLoader)
+	t.Run("with local file strategy", func(t *testing.T) {
+		loader := func(called *bool, pth *string) func(string) ([]byte, error) {
+			return func(p string) ([]byte, error) {
+				*called = true
+				*pth = p
+				return []byte(thisIsIt), nil
+			}
+		}
 
-	b, _ := ld("file:///a/c/myfile.yaml")
-	assert.True(t, called)
-	assert.Equal(t, "/a/c/myfile.yaml", pth)
-	assert.Equal(t, []byte(thisIsIt), b)
+		remLoader := func(p string) ([]byte, error) {
+			return []byte(thisIsNotIt), nil
+		}
 
-	called, pth = false, ""
-	b, _ = ld(`file://C:\a\c\myfile.yaml`)
-	assert.True(t, called)
-	assert.Equal(t, `C:\a\c\myfile.yaml`, pth)
-	assert.Equal(t, []byte(thisIsIt), b)
+		for _, toPin := range []strategyTest{
+			{
+				Title:           "valid fully qualified local URI, with rooted path",
+				Path:            "file:///a/c/myfile.yaml",
+				Expected:        "/a/c/myfile.yaml",
+				ExpectedWindows: `\a\c\myfile.yaml`,
+			},
+			{
+				Title:           "local URI with scheme, with host segment before path",
+				Path:            "file://a/c/myfile.yaml",
+				Expected:        "a/c/myfile.yaml",
+				ExpectedWindows: `\\a\c\myfile.yaml`, // UNC host
+			},
+			{
+				Title:           "local URI with scheme, with escaped characters",
+				Path:            "file://a/c/myfile%20%28x86%29.yaml",
+				Expected:        "a/c/myfile (x86).yaml",
+				ExpectedWindows: `\\a\c\myfile (x86).yaml`,
+			},
+			{
+				Title:           "local URI with scheme, rooted, with escaped characters",
+				Path:            "file:///a/c/myfile%20%28x86%29.yaml",
+				Expected:        "/a/c/myfile (x86).yaml",
+				ExpectedWindows: `\a\c\myfile (x86).yaml`,
+			},
+			{
+				Title:           "local URI with scheme, unescaped, with host",
+				Path:            "file://a/c/myfile (x86).yaml",
+				Expected:        "a/c/myfile (x86).yaml",
+				ExpectedWindows: `\\a\c\myfile (x86).yaml`,
+			},
+			{
+				Title:           "local URI with scheme, rooted, unescaped",
+				Path:            "file:///a/c/myfile (x86).yaml",
+				Expected:        "/a/c/myfile (x86).yaml",
+				ExpectedWindows: `\a\c\myfile (x86).yaml`,
+			},
+			{
+				Title:    "file URI with drive letter and backslashes, as a relative Windows path",
+				Path:     `file://C:\a\c\myfile.yaml`,
+				Expected: `C:\a\c\myfile.yaml`, // outcome on all platforms, not only windows
+			},
+			{
+				Title:           "file URI with drive letter and backslashes, as a rooted Windows path",
+				Path:            `file:///C:\a\c\myfile.yaml`,
+				Expected:        `/C:\a\c\myfile.yaml`, // on non-windows, this results most likely in a wrong path
+				ExpectedWindows: `C:\a\c\myfile.yaml`,  // on windows, we know that C: is a drive letter, so /C: becomes C:
+			},
+			{
+				Title:    "file URI with escaped backslashes",
+				Path:     `file://C%3A%5Ca%5Cc%5Cmyfile.yaml`,
+				Expected: `C:\a\c\myfile.yaml`, // outcome on all platforms, not only windows
+			},
+			{
+				Title:           "file URI with escaped backslashes, rooted",
+				Path:            `file:///C%3A%5Ca%5Cc%5Cmyfile.yaml`,
+				Expected:        `/C:\a\c\myfile.yaml`, // outcome on non-windows (most likely not a desired path)
+				ExpectedWindows: `C:\a\c\myfile.yaml`,  // outcome on windows
+			},
+			{
+				Title:           "URI with the file scheme, host omitted: relative path with extra dots",
+				Path:            `file://./a/c/d/../myfile.yaml`,
+				Expected:        `./a/c/d/../myfile.yaml`,
+				ExpectedWindows: `a\c\myfile.yaml`, // on windows, extra processing cleans the path
+			},
+			{
+				Title:           "relative URI without the file scheme, rooted path",
+				Path:            `/a/c/myfile.yaml`,
+				Expected:        `/a/c/myfile.yaml`,
+				ExpectedWindows: `\a\c\myfile.yaml`, // there is no drive letter, this would probably result in a wrong path on Windows
+			},
+			{
+				Title:           "relative URI without the file scheme, relative path",
+				Path:            `a/c/myfile.yaml`,
+				Expected:        `a/c/myfile.yaml`,
+				ExpectedWindows: `a\c\myfile.yaml`,
+			},
+			{
+				Title:           "relative URI without the file scheme, relative path with dots",
+				Path:            `./a/c/myfile.yaml`,
+				Expected:        `./a/c/myfile.yaml`,
+				ExpectedWindows: `.\a\c\myfile.yaml`,
+			},
+			{
+				Title:           "relative URI without the file scheme, relative path with extra dots",
+				Path:            `./a/c/../myfile.yaml`,
+				Expected:        `./a/c/../myfile.yaml`,
+				ExpectedWindows: `.\a\c\..\myfile.yaml`,
+			},
+			{
+				Title:           "relative URI without the file scheme, windows slashed-path with drive letter",
+				Path:            `A:/a/c/myfile.yaml`,
+				Expected:        `A:/a/c/myfile.yaml`, // on non-windows, this results most likely in a wrong path
+				ExpectedWindows: `A:\a\c\myfile.yaml`, // on windows, slashes are converted
+			},
+			{
+				Title:           "relative URI without the file scheme, windows backslashed-path with drive letter",
+				Path:            `A:\a\c\myfile.yaml`,
+				Expected:        `A:\a\c\myfile.yaml`, // on non-windows, this results most likely in a wrong path
+				ExpectedWindows: `A:\a\c\myfile.yaml`,
+			},
+			{
+				Title:           "URI with file scheme, host as Windows UNC name",
+				Path:            `file://host/share/folder/myfile.yaml`,
+				Expected:        `host/share/folder/myfile.yaml`,   // there is no host component accounted for
+				ExpectedWindows: `\\host\share\folder\myfile.yaml`, // on windows, the host is interpreted as an UNC host for a file share
+			},
+			// TODO: invalid URI (cannot unescape/parse path)
+		} {
+			tc := toPin
+			t.Run(tc.Title, func(t *testing.T) {
+				var (
+					called bool
+					pth    string
+				)
 
-	called, pth = false, ""
-	b, _ = ld(`file://C%3A%5Ca%5Cc%5Cmyfile.yaml`)
-	assert.True(t, called)
-	assert.Equal(t, `C:\a\c\myfile.yaml`, pth)
-	assert.Equal(t, []byte(thisIsIt), b)
+				ld := LoadStrategy("local", loader(&called, &pth), remLoader)
+				b, err := ld(tc.Path)
+				if tc.ExpectError {
+					require.Error(t, err)
+					assert.True(t, called)
+
+					return
+				}
+
+				require.NoError(t, err)
+				assert.True(t, called)
+				assert.Equal(t, []byte(thisIsIt), b)
+
+				if tc.ExpectedWindows != "" && runtime.GOOS == "windows" {
+					assert.Equalf(t, tc.ExpectedWindows, pth,
+						"expected local LoadStrategy(%q) to open: %q (windows)",
+						tc.Path, tc.ExpectedWindows,
+					)
+
+					return
+				}
+
+				assert.Equalf(t, tc.Expected, pth,
+					"expected local LoadStrategy(%q) to open: %q (any OS)",
+					tc.Path, tc.Expected,
+				)
+			})
+		}
+	})
 }
