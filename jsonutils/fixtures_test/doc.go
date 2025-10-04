a477bf1396292260673ae875629fd77281bc0c3f
Frederic BIDON <fredbi@yahoo.com>
2025-10-04 10:10:57 +0200

    licensing: modernized the licensing of source code.
    This commit replaces the full license terms in each go source file
    by the more compact SPDX annotations (i.e. 2 lines instead of 13).
    
    All go files now embed these summarized licensing terms.
    
    Also:
    * license is now cited in README
    
    Aside:
    * fixed sample code in README on how to register an adapter
    
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- jsonutils/fixtures_test/doc.go
+++ jsonutils/fixtures_test/doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2015-2025 go-swagger maintainers
+// SPDX-License-Identifier: Apache-2.0
+
 // Package fixtures exposes a test [Harness] utility
 // to test JSON and YAML transformations.
 package fixtures
